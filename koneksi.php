59763e19f316ee69f604cdfe995449ddf1a81ddc
Sitya <sitiaharyani@gmail.com>
2024-03-06 10:30:38 +0700

    update koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,20 @@
 <?php
+// $host = "localhost"; 
+// $username = "root";  
+// $password = "";  
+// $database = "db_ecat";  
+
 $host = "anzio-db.id.domainesia.com";//nama server	
 $username = "mandir36_staging";//usernya
 $password = "mandir36_staging";//password
-$database = "mandir36_mandir36_db_ecat_staging_staging";//database
+$database = "mandir36_db_ecat_staging";//database
 
 $host2 = "anzio-db.id.domainesia.com"; 
 $username2 = "mandir36_staging";  
 $password2 = "mandir36_staging";  
 $database2 = "mandir36_staging"; 
+
+
 
 // Buat koneksi ke database
 $koneksi = mysqli_connect($host, $username, $password, $database);
